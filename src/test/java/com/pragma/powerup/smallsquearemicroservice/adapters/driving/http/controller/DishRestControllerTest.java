303639afe2185ee4a04a315802b7360d506973f5
Nirsch95 <castromanzano95@gmail.com>
2023-05-27 20:50:03 -0500

    test: adjust tests

--- src/test/java/com/pragma/powerup/smallsquearemicroservice/adapters/driving/http/controller/DishRestControllerTest.java
+++ src/test/java/com/pragma/powerup/smallsquearemicroservice/adapters/driving/http/controller/DishRestControllerTest.java
@@ -20,7 +20,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class DishRestControllerTest {
+class DishRestControllerTest {
 
     private DishRestController dishRestController;
 
